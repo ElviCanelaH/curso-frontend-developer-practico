afa51db1e496c8450262bdc1d48aa3f6cf0795a6
ElviCanelaH <elvicanela11@gmail.com>
2023-07-21 22:17:49 -0600

    Menu Mobile Add

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const emailMenu = document.querySelector (".navbar-email");
 const desktopMenu = document.querySelector (".inactive");
+const hamMenuIcon = document.querySelector (".menu");
+const mobileMenu = document.querySelector (".mobile-menu");
 
 emailMenu.addEventListener("click", toggledesktopmenu);
+hamMenuIcon.addEventListener("click", togglemobilmenu);
+
 function toggledesktopmenu (){
   desktopMenu.classList.toggle("inactive");
 }
+
+
+function togglemobilmenu (){
+  mobileMenu.classList.toggle("inactive");
+}
+
+
+
+
+
+
